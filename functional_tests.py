d0567e4545df733eddc33e3fdd00c1661e8a171a
hillkm <hillk26@gator.uhd.edu>
2022-02-03 21:40:34 -0600

    Additional functional tests for site elements

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,15 +17,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # She reaches the correct homepage
         self.assertIn('To-Do', self.browser.title)
-        self.fail('finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # It's possible to immediately add to-do items
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do task'
+        )
 
         # She sets a new task: Investigate JNDI vuln
+        inputbox.send_keys('Investigate JNDI injection')
 
         # When she hits Enter, the page updates and lists the new task
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Investigate JNDI injection' for row in rows)
+        )
 
         # Typing into the text box again, she enters 'Check affected servers'
+        self.fail('Finish the test')
 
         # Both items should display properly
 
