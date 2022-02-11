7755601df847c1a3b810f754fc7f6f9a4c489a50
hillkm <hillk26@gator.uhd.edu>
2022-02-11 03:39:36 -0600

    hack ALLOWED_HOSTS to be * for testing

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!m@1yjqdv7__!5ikv2mf^$it-e%^=o*7n9^=yzgih)%ebfuh))'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
